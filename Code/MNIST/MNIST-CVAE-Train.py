f66f144fab2f08f3c2bcf5333e5b13787c21fe66
owilo <noye.valentin@gmail.com>
2025-02-24 17:03:53 +0100

    CVAE Update

--- Code/MNIST/MNIST-CVAE-Train.py
+++ Code/MNIST/MNIST-CVAE-Train.py
@@ -3,6 +3,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
 
 class Sampling(layers.Layer):
     def call(self, inputs):
@@ -17,30 +18,31 @@ class Encoder(layers.Layer):
         super().__init__(**kwargs)
         self.conv1 = layers.Conv2D(32, 3, strides=2, activation='relu', padding='same')
         self.conv2 = layers.Conv2D(64, 3, strides=2, activation='relu', padding='same')
+        self.conv3 = layers.Conv2D(64, 3, strides=2, activation='relu', padding='same')
         self.flatten = layers.Flatten()
-        self.concat = layers.Concatenate()
         self.dense = layers.Dense(256, activation='relu')
         self.z_mean = layers.Dense(latent_dim)
         self.z_log_var = layers.Dense(latent_dim)
         self.sampling = Sampling()
 
-    def call(self, inputs):
-        x, y = inputs
+    def call(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
+        x = self.conv3(x)
         x = self.flatten(x)
-        x = self.concat([x, y])
         x = self.dense(x)
         z_mean = self.z_mean(x)
         z_log_var = self.z_log_var(x)
-        return z_mean, z_log_var, self.sampling((z_mean, z_log_var))
+        z = self.sampling((z_mean, z_log_var))
+        return z_mean, z_log_var, z
 
 class Decoder(layers.Layer):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.concat = layers.Concatenate()
+        self.dense0 = layers.Dense(256, activation='relu')
         self.dense = layers.Dense(7*7*64, activation='relu')
-        self.reshape = layers.Reshape((7,7,64))
+        self.reshape = layers.Reshape((7, 7, 64))
         self.deconv1 = layers.Conv2DTranspose(64, 3, strides=2, activation='relu', padding='same')
         self.deconv2 = layers.Conv2DTranspose(32, 3, strides=2, activation='relu', padding='same')
         self.conv_out = layers.Conv2D(1, 3, activation='sigmoid', padding='same')
@@ -48,6 +50,7 @@ class Decoder(layers.Layer):
     def call(self, inputs):
         z, y = inputs
         x = self.concat([z, y])
+        x = self.dense0(x)
         x = self.dense(x)
         x = self.reshape(x)
         x = self.deconv1(x)
@@ -74,10 +77,11 @@ class CVAE(keras.Model):
             self.kl_loss_tracker,
         ]
     
+    @tf.function
     def train_step(self, data):
         x, y = data
         with tf.GradientTape() as tape:
-            z_mean, z_log_var, z = self.encoder((x, y))
+            z_mean, z_log_var, z = self.encoder(x)
             x_recon = self.decoder((z, y))
             reconstruction_loss = tf.reduce_mean(
                 tf.reduce_sum(
@@ -105,9 +109,10 @@ class CVAE(keras.Model):
             "beta": beta
         }
     
+    @tf.function
     def test_step(self, data):
         x, y = data
-        z_mean, z_log_var, z = self.encoder((x, y))
+        z_mean, z_log_var, z = self.encoder(x)
         x_recon = self.decoder((z, y))
         reconstruction_loss = tf.reduce_mean(
             tf.reduce_sum(
@@ -132,7 +137,7 @@ class CVAE(keras.Model):
     
     def call(self, inputs):
         x, y = inputs
-        z_mean, z_log_var, z = self.encoder((x, y))
+        z_mean, z_log_var, z = self.encoder(x)
         return self.decoder((z, y))
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
@@ -145,8 +150,8 @@ num_classes = 10
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-latent_dim = 16
-cvae = CVAE(latent_dim=latent_dim, final_beta=3.5, annealing_steps=10000)
+latent_dim = 24
+cvae = CVAE(latent_dim=latent_dim, final_beta=3.0, annealing_steps=20000)
 cvae.compile(optimizer=keras.optimizers.Adam())
 
 cvae.fit(
@@ -155,11 +160,6 @@ cvae.fit(
     epochs=30,
     validation_data=(x_test, y_test)
 )
-
-x0 = x_test[0:1]
-y0 = y_test[0:1]
-
-z_mean, z_log_var, z = cvae.encoder((x0, y0))
 
 digits = [
     1333,  # 0
@@ -180,7 +180,7 @@ for j, idx in enumerate(digits):
     x_src = x_test[idx:idx + 1]
     y_src = y_test[idx:idx + 1]
     
-    z_mean, z_log_var, z = cvae.encoder((x_src, y_src))
+    _, _, z = cvae.encoder(x_src)
     
     axs[j, 0].imshow(x_src[0, :, :, 0], cmap='gray')
     axs[j, 0].axis('off')
@@ -197,3 +197,34 @@ for j, idx in enumerate(digits):
 plt.suptitle("CVAE", fontsize=16)
 plt.tight_layout()
 plt.savefig("./Results/mnist-cvae.png")
+
+z_mean, _, _ = cvae.encoder(x_test)
+z_mean = z_mean.numpy()
+
+tsne = TSNE(n_components=2, random_state=42)
+z_tsne = tsne.fit_transform(z_mean)
+
+labels = np.argmax(y_test, axis=1)
+
+plt.figure(figsize=(8, 8))
+scatter = plt.scatter(z_tsne[:, 0], z_tsne[:, 1], c=labels, cmap="Paired", alpha=0.35)
+plt.colorbar(scatter, ticks=range(10))
+plt.title("t-SNE CVAE (style)")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.savefig("./Results/mnist-cvae-style-tsne.png")
+
+z_mean_class = np.concatenate((z_mean, y_test.astype(float)), axis=1)
+
+tsne = TSNE(n_components=2, random_state=42)
+z_tsne = tsne.fit_transform(z_mean_class)
+
+labels = np.argmax(y_test, axis=1)
+
+plt.figure(figsize=(8, 8))
+scatter = plt.scatter(z_tsne[:, 0], z_tsne[:, 1], c=labels, cmap="Paired", alpha=0.35)
+plt.colorbar(scatter, ticks=range(10))
+plt.title("t-SNE CVAE")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.savefig("./Results/mnist-cvae-tsne.png")
