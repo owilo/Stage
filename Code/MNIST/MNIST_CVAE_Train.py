88131643873a3c81ad50afae9d8cd2bbce5afe66
owilo <noye.valentin@gmail.com>
2025-02-25 11:34:34 +0100

    Decoupling CVAE train & test + Interactive style editor

--- Code/MNIST/MNIST_CVAE_Train.py
+++ Code/MNIST/MNIST_CVAE_Train.py
@@ -2,9 +2,8 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
 
+@tf.keras.utils.register_keras_serializable()
 class Sampling(layers.Layer):
     def call(self, inputs):
         z_mean, z_log_var = inputs
@@ -13,9 +12,19 @@ class Sampling(layers.Layer):
         epsilon = tf.random.normal(shape=(batch, dim))
         return z_mean + tf.exp(0.5 * z_log_var) * epsilon
 
+    def get_config(self):
+        config = super().get_config()
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+@tf.keras.utils.register_keras_serializable()
 class Encoder(layers.Layer):
     def __init__(self, latent_dim=2, **kwargs):
         super().__init__(**kwargs)
+        self.latent_dim = latent_dim
         self.conv1 = layers.Conv2D(32, 3, strides=2, activation='relu', padding='same')
         self.conv2 = layers.Conv2D(64, 3, strides=2, activation='relu', padding='same')
         self.conv3 = layers.Conv2D(64, 3, strides=2, activation='relu', padding='same')
@@ -36,6 +45,16 @@ class Encoder(layers.Layer):
         z = self.sampling((z_mean, z_log_var))
         return z_mean, z_log_var, z
 
+    def get_config(self):
+        config = super().get_config()
+        config.update({"latent_dim": self.latent_dim})
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+@tf.keras.utils.register_keras_serializable()
 class Decoder(layers.Layer):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -57,6 +76,15 @@ class Decoder(layers.Layer):
         x = self.deconv2(x)
         return self.conv_out(x)
 
+    def get_config(self):
+        config = super().get_config()
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+@tf.keras.utils.register_keras_serializable()
 class CVAE(keras.Model):
     def __init__(self, latent_dim=2, final_beta=4.0, annealing_steps=6000, **kwargs):
         super(CVAE, self).__init__(**kwargs)
@@ -139,92 +167,45 @@ class CVAE(keras.Model):
         x, y = inputs
         z_mean, z_log_var, z = self.encoder(x)
         return self.decoder((z, y))
-
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-x_train = x_train.astype("float32") / 255.
-x_train = np.expand_dims(x_train, -1)
-x_test = x_test.astype("float32") / 255.
-x_test = np.expand_dims(x_test, -1)
-
-num_classes = 10
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
-latent_dim = 24
-cvae = CVAE(latent_dim=latent_dim, final_beta=3.0, annealing_steps=20000)
-cvae.compile(optimizer=keras.optimizers.Adam())
-
-cvae.fit(
-    x=x_train, y=y_train,
-    batch_size=32,
-    epochs=30,
-    validation_data=(x_test, y_test)
-)
-
-digits = [
-    1333,  # 0
-    9415,  # 1
-    3773,  # 2
-    524,   # 3
-    1980,  # 4
-    1874,  # 5
-    4252,  # 6
-    6960,  # 7
-    8466,  # 8
-    5333   # 9
-]
-
-fig, axs = plt.subplots(10, 11, figsize=(11, 10))
-
-for j, idx in enumerate(digits):
-    x_src = x_test[idx:idx + 1]
-    y_src = y_test[idx:idx + 1]
     
-    _, _, z = cvae.encoder(x_src)
-    
-    axs[j, 0].imshow(x_src[0, :, :, 0], cmap='gray')
-    axs[j, 0].axis('off')
-    
-    for i in range(10):
-        y_target = np.zeros((1, num_classes), dtype="float32")
-        y_target[0, i] = 1.0
-        
-        x_decoded = cvae.decoder((z, y_target))
-        
-        axs[j, i + 1].imshow(x_decoded[0, :, :, 0], cmap='gray')
-        axs[j, i + 1].axis('off')
+    def get_config(self):
+        config = super(CVAE, self).get_config()
+        config.update({
+            "latent_dim": self.latent_dim,
+            "final_beta": self.final_beta,
+            "annealing_steps": self.annealing_steps,
+        })
+        return config
 
-plt.suptitle("CVAE", fontsize=16)
-plt.tight_layout()
-plt.savefig("./Results/mnist-cvae.png")
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
 
-z_mean, _, _ = cvae.encoder(x_test)
-z_mean = z_mean.numpy()
+if __name__ == "__main__":
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    x_train = x_train.astype("float32") / 255.
+    x_train = np.expand_dims(x_train, -1)
+    x_test = x_test.astype("float32") / 255.
+    x_test = np.expand_dims(x_test, -1)
 
-tsne = TSNE(n_components=2, random_state=42)
-z_tsne = tsne.fit_transform(z_mean)
+    num_classes = 10
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
 
-labels = np.argmax(y_test, axis=1)
+    latent_dim = 16
+    cvae = CVAE(latent_dim=latent_dim, final_beta=3.5, annealing_steps=25000)
+    cvae.compile(optimizer=keras.optimizers.Adam())
 
-plt.figure(figsize=(8, 8))
-scatter = plt.scatter(z_tsne[:, 0], z_tsne[:, 1], c=labels, cmap="Paired", alpha=0.35)
-plt.colorbar(scatter, ticks=range(10))
-plt.title("t-SNE CVAE (style)")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.savefig("./Results/mnist-cvae-style-tsne.png")
+    cvae.fit(
+        x=x_train, y=y_train,
+        batch_size=32,
+        epochs=30,
+        validation_data=(x_test, y_test)
+    )
 
-z_mean_class = np.concatenate((z_mean, y_test.astype(float)), axis=1)
+    dummy_x = np.random.rand(1, 28, 28, 1).astype("float32")
+    dummy_y = np.zeros((1, 10)).astype("float32")
 
-tsne = TSNE(n_components=2, random_state=42)
-z_tsne = tsne.fit_transform(z_mean_class)
+    _ = cvae((dummy_x, dummy_y))
 
-labels = np.argmax(y_test, axis=1)
-
-plt.figure(figsize=(8, 8))
-scatter = plt.scatter(z_tsne[:, 0], z_tsne[:, 1], c=labels, cmap="Paired", alpha=0.35)
-plt.colorbar(scatter, ticks=range(10))
-plt.title("t-SNE CVAE")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.savefig("./Results/mnist-cvae-tsne.png")
+    cvae.save("./Models/CVAE/cvae16.keras")
