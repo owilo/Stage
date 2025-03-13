ac5299797849c00c3fe1cbf399b7d54bbfa2ab3e
owilo <noye.valentin@gmail.com>
2025-03-13 10:13:59 +0100

    CVAE half dataset training

---
+++ Code/MNIST/MNIST_CVAE_Half_Dataset_Train.py
@@ -0,0 +1,222 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import numpy as np
+
+@tf.keras.utils.register_keras_serializable()
+class Sampling(layers.Layer):
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.random.normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+    def get_config(self):
+        config = super().get_config()
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+@tf.keras.utils.register_keras_serializable()
+class Encoder(layers.Layer):
+    def __init__(self, latent_dim=2, **kwargs):
+        super().__init__(**kwargs)
+        self.latent_dim = latent_dim
+        self.conv1 = layers.Conv2D(32, 3, strides=2, activation='relu', padding='same')
+        self.conv2 = layers.Conv2D(64, 3, strides=2, activation='relu', padding='same')
+        self.conv3 = layers.Conv2D(64, 3, strides=2, activation='relu', padding='same')
+        self.flatten = layers.Flatten()
+        self.dense = layers.Dense(256, activation='relu')
+        self.z_mean = layers.Dense(latent_dim)
+        self.z_log_var = layers.Dense(latent_dim)
+        self.sampling = Sampling()
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.flatten(x)
+        x = self.dense(x)
+        z_mean = self.z_mean(x)
+        z_log_var = self.z_log_var(x)
+        z = self.sampling((z_mean, z_log_var))
+        return z_mean, z_log_var, z
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"latent_dim": self.latent_dim})
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+@tf.keras.utils.register_keras_serializable()
+class Decoder(layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.concat = layers.Concatenate()
+        self.dense0 = layers.Dense(256, activation='relu')
+        self.dense = layers.Dense(7*7*64, activation='relu')
+        self.reshape = layers.Reshape((7, 7, 64))
+        self.deconv1 = layers.Conv2DTranspose(64, 3, strides=2, activation='relu', padding='same')
+        self.deconv2 = layers.Conv2DTranspose(32, 3, strides=2, activation='relu', padding='same')
+        self.conv_out = layers.Conv2D(1, 3, activation='sigmoid', padding='same')
+
+    def call(self, inputs):
+        z, y = inputs
+        x = self.concat([z, y])
+        x = self.dense0(x)
+        x = self.dense(x)
+        x = self.reshape(x)
+        x = self.deconv1(x)
+        x = self.deconv2(x)
+        return self.conv_out(x)
+
+    def get_config(self):
+        config = super().get_config()
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+@tf.keras.utils.register_keras_serializable()
+class CVAE(keras.Model):
+    def __init__(self, latent_dim=2, final_beta=4.0, annealing_steps=6000, **kwargs):
+        super(CVAE, self).__init__(**kwargs)
+        self.latent_dim = latent_dim
+        self.final_beta = final_beta
+        self.annealing_steps = annealing_steps
+        self.encoder = Encoder(latent_dim=latent_dim)
+        self.decoder = Decoder()
+        self.total_loss_tracker = keras.metrics.Mean(name="loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(name="reconstruction_loss")
+        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+    
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker,
+        ]
+    
+    @tf.function
+    def train_step(self, data):
+        x, y = data
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(x)
+            x_recon = self.decoder((z, y))
+            reconstruction_loss = tf.reduce_mean(
+                tf.reduce_sum(
+                    keras.losses.binary_crossentropy(x, x_recon), axis=(1, 2)
+                )
+            )
+            step = tf.cast(self.optimizer.iterations, tf.float32)
+            beta = self.final_beta * tf.minimum(1.0, step / self.annealing_steps)
+            kl_loss = beta * (-0.5 * tf.reduce_mean(
+                tf.reduce_sum(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=1)
+            ))
+            total_loss = reconstruction_loss + kl_loss
+        
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+            "beta": beta
+        }
+    
+    @tf.function
+    def test_step(self, data):
+        x, y = data
+        z_mean, z_log_var, z = self.encoder(x)
+        x_recon = self.decoder((z, y))
+        reconstruction_loss = tf.reduce_mean(
+            tf.reduce_sum(
+                keras.losses.binary_crossentropy(x, x_recon), axis=(1, 2)
+            )
+        )
+        beta = self.final_beta
+        kl_loss = beta * (-0.5 * tf.reduce_mean(
+            tf.reduce_sum(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=1)
+        ))
+        total_loss = reconstruction_loss + kl_loss
+        
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
+    
+    def call(self, inputs):
+        x, y = inputs
+        z_mean, z_log_var, z = self.encoder(x)
+        return self.decoder((z, y))
+    
+    def get_config(self):
+        config = super(CVAE, self).get_config()
+        config.update({
+            "latent_dim": self.latent_dim,
+            "final_beta": self.final_beta,
+            "annealing_steps": self.annealing_steps,
+        })
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+if __name__ == "__main__":
+    np.random.seed(42)
+
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    x_train = x_train.astype("float32") / 255.
+    x_train = np.expand_dims(x_train, -1)
+    x_test = x_test.astype("float32") / 255.
+    x_test = np.expand_dims(x_test, -1)
+
+    idx1 = np.concatenate([np.where(y_train == c)[0][:len(np.where(y_train == c)[0]) // 2] for c in range(10)])
+    idx2 = np.concatenate([np.where(y_train == c)[0][len(np.where(y_train == c)[0]) // 2:] for c in range(10)])
+
+    idx1 = tf.convert_to_tensor(idx1, dtype=tf.int32)
+    idx2 = tf.convert_to_tensor(idx2, dtype=tf.int32)
+
+    x_split1 = tf.gather(x_train, idx1)
+    y_split1 = tf.gather(y_train, idx1)
+
+    num_classes = 10
+    y_split1 = keras.utils.to_categorical(y_split1, num_classes)
+
+    latent_dim = 16
+    cvae = CVAE(latent_dim=latent_dim, final_beta=3.5, annealing_steps=25000)
+    cvae.compile(optimizer=keras.optimizers.Adam())
+
+    cvae.fit(
+        x=x_split1,
+        y=y_split1,
+        batch_size=32,
+        epochs=50,
+        validation_split = 0.1
+    )
+
+    dummy_x = np.random.rand(1, 28, 28, 1).astype("float32")
+    dummy_y = np.zeros((1, 10)).astype("float32")
+
+    _ = cvae((dummy_x, dummy_y))
+
+    cvae.save("./Models/CVAE/h-cvae16.keras")
